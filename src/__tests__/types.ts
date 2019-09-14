18749d18b598fc6e79f2250c008ada2621f11361
Tamino Martinius <dev@zaku.eu>
2019-09-14 23:05:55 +0200

    Update test types

--- src/__tests__/types.ts
+++ src/__tests__/types.ts
@@ -1,34 +1,20 @@
-import Knex from 'knex';
-import {
-  Filter,
-  Identifiable,
-} from '@next-model/core';
+import * as Knex from 'knex';
+
+import { Filter } from '@next-model/core';
 
 export interface Context {
   definitions: () => void;
   tests: () => void;
-  reset?: () => void,
-};
-
-export const it = test;
-
-export const context = (description: string, { definitions, tests, reset }: Context) => {
-  describe(description, () => {
-    beforeEach(definitions);
-    tests();
-    if (reset !== undefined) {
-      afterEach(reset);
-    }
-  });
-};
+  reset?: () => void;
+}
 
 export type Connection = Knex.Sqlite3ConnectionConfig | Knex.MySqlConnectionConfig;
 
 export interface FilterSpecs {
-  filter: () => Filter<any>;
-  results: (() => number[]) | string
-};
+  filter: () => Filter<any> | undefined;
+  results: (() => number[]) | string;
+}
 
 export interface FilterSpecGroup {
   [key: string]: FilterSpecs[];
-};
+}
